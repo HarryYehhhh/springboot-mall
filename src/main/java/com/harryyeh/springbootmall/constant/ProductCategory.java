2764f6480ee208070154cb9a1ff767a747f1d903
harrdicker <harry614099@gmail.com>
2022-09-25 01:20:04 +0800

    商品功能 Refactor category, 使用enum

---
+++ src/main/java/com/harryyeh/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,8 @@
+package com.harryyeh.springbootmall.constant;
+
+public enum ProductCategory {
+    //必用大寫
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- src/main/java/com/harryyeh/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/harryyeh/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,18 +1,25 @@
 package com.harryyeh.springbootmall.rowmapper;
 
+import com.harryyeh.springbootmall.constant.ProductCategory;
 import com.harryyeh.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ProductRowMapper implements RowMapper{
+public class ProductRowMapper implements RowMapper<Product>{
     @Override
     public Product mapRow(ResultSet resultSet, int i) throws SQLException {
         Product product = new Product();
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        String categoryStr = resultSet.getString("category");//String轉enum
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+//        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
